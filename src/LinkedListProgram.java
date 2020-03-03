647448a20ec120d6fecc058d9943c2d2c16b8ea3
write55 <42893610+write55@users.noreply.github.com>
2020-03-03 08:31:53 -0500

    Add files via upload

--- src/LinkedListProgram.java
+++ src/LinkedListProgram.java
@@ -1,99 +1,131 @@
 
 public class LinkedListProgram<E extends Comparable<E>> {
 
-    private ListNode<E> front;
+	/**
+	 * front - first ListNode object in linked list
+	 */
+	private ListNode<E> front;
 
-    public LinkedListProgram() {
-        front = null;
-    }
+	/**
+	 * Constructor for LinkedListProgram object
+	 */
+	public LinkedListProgram() {
+		front = null;
+	}
 
-    public ListNode<E> getFront() {
-        return front;
-    }
+	public ListNode<E> getFront() {
+		return front;
+	}
 
-    public void setFront(ListNode<E> front) {
-        this.front = front;
-    }
+	public void setFront(ListNode<E> front) {
+		this.front = front;
+	}
 
-    public void add(ListNode<E> input) {
-        if (front == null) {
-            front = input;
-        } else if (input.compareTo(front) < 0) {
-            input.setNext(front);
-            front = input;
-        } else if (input.equals(front)) {
-            System.out.println("Already on List");
-        } else {
-            ListNode<E> current = front;
-            ListNode<E> previous = null;
-            while (current != null && input.compareTo(current) > 0) {
-                previous = current;
-                current = current.getNext();
-            }
-            if (current == null) {
-                previous.setNext(input);
-            } else if (!input.equals(current)) {
-                input.setNext(current);
-                previous.setNext(input);
-            } else {
-                System.out.println("Already on List");
-            }
-        }
-    }
+	/**
+	 * Adds a given ListNode object to the linked list in order
+	 * 
+	 * @param input
+	 *            ListNode object to be added in order
+	 */
+	public void add(ListNode<E> input) {
+		if (front == null) {
+			front = input;
+		} else if (input.compareTo(front) < 0) {
+			input.setNext(front);
+			front = input;
+		} else if (input.equals(front)) {
+			System.out.println("Already on List");
+		} else {
+			ListNode<E> current = front;
+			ListNode<E> previous = null;
+			while (current != null && input.compareTo(current) > 0) {
+				previous = current;
+				current = current.getNext();
+			}
+			if (current == null) {
+				previous.setNext(input);
+			} else if (!input.equals(current)) {
+				input.setNext(current);
+				previous.setNext(input);
+			} else {
+				System.out.println("Already on List");
+			}
+		}
+	}
 
-    public void delete(ListNode<E> input) {
-        if (front == null) {
-            System.out.println("List Empty");
-        } else if (input.equals(front)) {
-            front = front.getNext();
-        } else {
-            ListNode<E> current = front;
-            ListNode<E> previous = null;
-            while (current.getNext() != null && !input.equals(current)) {
-                previous = current;
-                current = current.getNext();
-            }
-            if (input.equals(current)) {
-                if (current.getNext() == null) {
-                    previous.setNext(null);
-                } else {
-                    previous.setNext(current.getNext());
-                }
-            } else {
-                System.out.println("Not on List");
-            }
-        }
-    }
+	/**
+	 * Deletes a given ListNode object from the linked list, keeping the list in
+	 * order
+	 * 
+	 * @param input
+	 *            ListNode object to be removed if present
+	 */
+	public void delete(ListNode<E> input) {
+		if (front == null) {
+			System.out.println("List Empty");
+		} else if (input.equals(front)) {
+			front = front.getNext();
+		} else {
+			ListNode<E> current = front;
+			ListNode<E> previous = null;
+			while (current.getNext() != null && !input.equals(current)) {
+				previous = current;
+				current = current.getNext();
+			}
+			if (input.equals(current)) {
+				if (current.getNext() == null) {
+					previous.setNext(null);
+				} else {
+					previous.setNext(current.getNext());
+				}
+			} else {
+				System.out.println("Not on List");
+			}
+		}
+	}
 
-    public void deleteList() {
-        front = null;
-    }
+	/**
+	 * Clears the entire linked list
+	 */
+	public void deleteList() {
+		front = null;
+	}
 
-    public ListNode<E> reverseList(ListNode<E> reverseThis) {
-        if (reverseThis == null || reverseThis.getNext() == null) {
-            front = reverseThis;
-            return reverseThis;
-        }
-        ListNode<E> temp = reverseList(reverseThis.getNext());
-        reverseThis.getNext().setNext(reverseThis);
-        reverseThis.setNext(null);
-        return temp;
-    }
+	/**
+	 * Reverses the linked list
+	 * 
+	 * @param reverseThis
+	 *            front ListNode of the linked list to be reversed
+	 * @return ListNode object, only used in the method for recursion
+	 */
+	public ListNode<E> reverseList(ListNode<E> reverseThis) {
+		if (reverseThis == null || reverseThis.getNext() == null) {
+			front = reverseThis;
+			return reverseThis;
+		}
+		ListNode<E> temp = reverseList(reverseThis.getNext());
+		reverseThis.getNext().setNext(reverseThis);
+		reverseThis.setNext(null);
+		return temp;
+	}
 
-    public void printList() {
-        if (front == null) {
-            System.out.println("List is Empty");
-        } else {
-            {
-                System.out.print("[ ");
-                ListNode<E> n = front;
-                while (n != null) {
-                    System.out.print(n.getData() + " ");
-                    n = n.getNext();
-                }
-                System.out.println("]");
-            }
-        }
-    }
+	/**
+	 * Prints out the linked list in a readable format
+	 */
+	public void printList() {
+		if (front == null) {
+			System.out.println("List is Empty");
+		} else {
+			{
+				System.out.print("[ ");
+				ListNode<E> n = front;
+				while (n != null) {
+					System.out.print(n.getData() + " ");
+					n = n.getNext();
+				}
+				System.out.println("]");
+			}
+		}
+	}
 
 }
--- src/LinkedListRunner.java
+++ src/LinkedListRunner.java
@@ -4,80 +4,120 @@ import java.io.InputStreamReader;
 
 public class LinkedListRunner<E extends Comparable<E>> {
 
-    public static ListNode<Integer> input() throws NumberFormatException, IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter an Integer: ");
-        Integer input = Integer.valueOf(in.readLine());
-        // Integer input = new Integer(in.readLine());
-        return new ListNode<Integer>(input, null);
-    }
+	/**
+	 * Creates a new ListNode object with an Integer in the data field, taking
+	 * user input
+	 * 
+	 * @return ListNode object with next = null and given Integer data
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
+	public static ListNode<Integer> input() throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter an Integer: ");
+		Integer input = Integer.valueOf(in.readLine());
+		// Integer input = new Integer(in.readLine());
+		return new ListNode<Integer>(input, null);
+	}
 
-    public static void add(LinkedListProgram<Integer> list) {
-        try {
-            list.add(input());
-        } catch (Exception E) {
-            System.out.println("Input error, try again");
-        }
-    }
+	/**
+	 * Calls the add method on a given LinkedListProgram object with a ListNode
+	 * object from input()
+	 * 
+	 * @param list
+	 *            LinkedListProgram object, the given linked list which a new
+	 *            ListNode object is added to
+	 */
+	public static void add(LinkedListProgram<Integer> list) {
+		try {
+			list.add(input());
+		} catch (Exception E) {
+			System.out.println("Input error, try again");
+		}
+	}
 
-    public static void delete(LinkedListProgram<Integer> list) {
-        try {
-            list.delete(input());
-        } catch (Exception E) {
-            System.out.println("Input error, try again");
-        }
-    }
+	/**
+	 * Calls the delete method on a given LinkedListProgram object with a
+	 * ListNode object from input()
+	 * 
+	 * @param list
+	 *            LinkedListProgram object, the given linked list which a new
+	 *            ListNode object is deleted from
+	 */
+	public static void delete(LinkedListProgram<Integer> list) {
+		try {
+			list.delete(input());
+		} catch (Exception E) {
+			System.out.println("Input error, try again");
+		}
+	}
 
-    public static void run(char input, LinkedListProgram<Integer> list) {
-        switch (input) {
-            case 'A':
-                add(list);
-                break;
-            case 'D':
-                delete(list);
-                break;
-            case 'C':
-                list.deleteList();
-                System.out.println("List Cleared");
-                break;
-            case 'P':
-                list.printList();
-                break;
-            case 'R':
-                list.reverseList(list.getFront());
-                break;
-            default:
-                System.out.println("Invalid command, try again");
-                break;
-        }
-    }
+	/**
+	 * Runs an action on a given LinkedListProgram object depending on the given
+	 * command
+	 * 
+	 * @param input
+	 *            character for a given command
+	 * @param list
+	 *            LinkedListProgram object to run an action on
+	 */
+	public static void run(char input, LinkedListProgram<Integer> list) {
+		switch (input) {
+		case 'A':
+			add(list);
+			break;
+		case 'D':
+			delete(list);
+			break;
+		case 'C':
+			list.deleteList();
+			System.out.println("List Cleared");
+			break;
+		case 'P':
+			list.printList();
+			break;
+		case 'R':
+			list.reverseList(list.getFront());
+			break;
+		default:
+			System.out.println("Invalid command, try again");
+			break;
+		}
+	}
 
-    public static char getInput() throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("\nEnter a Command: ");
-        String input = in.readLine().toUpperCase();
-        while (input.isEmpty()) {
-            System.out.println("Nothing entered, try again");
-            System.out.print("\nEnter a Command: ");
-            input = in.readLine().toUpperCase();
-        }
-        return input.charAt(0);
-    }
+	/**
+	 * Gets user input for a character to be used in run()
+	 * 
+	 * @return character from user input
+	 * @throws IOException
+	 */
+	public static char getInput() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("\nEnter a Command: ");
+		String input = in.readLine().toUpperCase();
+		while (input.isEmpty()) {
+			System.out.println("Nothing entered, try again");
+			System.out.print("\nEnter a Command: ");
+			input = in.readLine().toUpperCase();
+		}
+		return input.charAt(0);
+	}
 
-    public static void main(String[] args) throws IOException {
-        LinkedListProgram<Integer> list = new LinkedListProgram<>();
-        System.out.println(
-                "\nCommands: \nA: Add Node \nD: Delete Node \nC: Clear List \nP: Print List \nR: Reverse List \nQ: Quit");
-        char input = getInput();
-        while (input != 'Q') {
-            run(input, list);
-            input = getInput();
-        }
-        System.out.println("Program Complete, Quitting");
+	public static void main(String[] args) throws IOException {
+		LinkedListProgram<Integer> list = new LinkedListProgram<>();
+		System.out.println(
+				"\nCommands: \nA: Add Node \nD: Delete Node \nC: Clear List \nP: Print List \nR: Reverse List \nQ: Quit");
+		char input = getInput();
+		while (input != 'Q') {
+			run(input, list);
+			input = getInput();
+		}
+		System.out.println("Program Complete, Quitting");
 
-    }
+	}
 
 }
+
 
 
 //RUN 1
@@ -135,10 +175,10 @@ Already on List
 
 Enter a Command:D
 Enter an Integer: 15
-Input error, try again
 
 Enter a Command:D
 Enter an Integer: x
+Input error, try again
 
 Enter a Command:D
 Enter an Integer: 10
