00365394ba5f97b1d191d11fddf85656b9df789f
write55 <writeusernamehere55@gmail.com>
2020-03-07 18:27:15 -0500

    LinkedList Program

--- src/LinkedListProgram.java
+++ src/LinkedListProgram.java
@@ -1,200 +1,197 @@
 
 public class LinkedListProgram<E extends Comparable<E>> {
 
-	/**
-	 * front - first ListNode object in linked list
-	 */
-	private ListNode<E> front;
-	/**
-	 * back - last ListNode object
-	 */
-	private ListNode<E> back;
+    /**
+     * front - first ListNode object in linked list
+     */
+    private ListNode<E> front;
+    /**
+     * back - last ListNode object
+     */
+    private ListNode<E> back;
 
-	/**
-	 * Constructor for LinkedListProgram object
-	 */
-	public LinkedListProgram() {
-		front = null;
-	}
+    /**
+     * Constructor for LinkedListProgram object
+     */
+    public LinkedListProgram() {
+        front = null;
+    }
 
-	public ListNode<E> getFront() {
-		return front;
-	}
+    public ListNode<E> getFront() {
+        return front;
+    }
 
-	public void setFront(ListNode<E> front) {
-		this.front = front;
-	}
+    public void setFront(ListNode<E> front) {
+        this.front = front;
+    }
 
-	/**
-	 * Adds a given ListNode object to the linked list in order
-	 * 
-	 * @param input
-	 *            ListNode object to be added in order
-	 */
-	public void add(ListNode<E> input, ListNode<E> mid) {
-		if (front == null && back == null) {
-			front = input;
-			back = input;
-		} else if (input.compareTo(front) < 0) {
-			input.setNext(front);
-			front = input;
-			input.getNext().setPrevious(front);
-		} else if (input.compareTo(back) > 0) {
-			input.setPrevious(back);
-			back = input;
-			input.getPrevious().setNext(back);
-		} else if (input.equals(front) || input.equals(back)) {
-			System.out.println("Already on List");
-		} else {
-			if (input.compareTo(mid) <= 0) {
-				forwardsAdd(input);
-			} else {
-				backwardsAdd(input);
-			}
-		}
-	}
+    /**
+     * Adds a given ListNode object to the linked list in order
+     *
+     * @param input ListNode object to be added in order
+     */
+    public void add(ListNode<E> input, ListNode<E> mid) {
+        if (front == null && back == null) {
+            front = input;
+            back = input;
+        } else if (input.compareTo(front) < 0) {
+            input.setNext(front);
+            front = input;
+            input.getNext().setPrevious(front);
+        } else if (input.compareTo(back) > 0) {
+            input.setPrevious(back);
+            back = input;
+            input.getPrevious().setNext(back);
+        } else if (input.equals(front) || input.equals(back)) {
+            System.out.println("Already on List");
+        } else {
+            if (input.compareTo(mid) <= 0) {
+                forwardsAdd(input);
+            } else {
+                backwardsAdd(input);
+            }
+        }
+    }
 
-	public void forwardsAdd(ListNode<E> input) {
-		ListNode<E> current = front;
-		ListNode<E> previous = null;
-		while (input.compareTo(current) > 0) {
-			previous = current;
-			current = current.getNext();
-		}
-		if (input.equals(current)) {
-			System.out.println("Already on List");
-		} else {
-			input.setNext(current);
-			current.setPrevious(input);
-			previous.setNext(input);
-			input.setPrevious(previous);
-		}
-	}
+    public void forwardsAdd(ListNode<E> input) {
+        ListNode<E> current = front;
+        ListNode<E> previous = null;
+        while (input.compareTo(current) > 0) {
+            previous = current;
+            current = current.getNext();
+        }
+        if (input.equals(current)) {
+            System.out.println("Already on List");
+        } else {
+            input.setNext(current);
+            current.setPrevious(input);
+            previous.setNext(input);
+            input.setPrevious(previous);
+        }
+    }
 
-	public void backwardsAdd(ListNode<E> input) {
-		ListNode<E> current = back;
-		ListNode<E> previous = null;
-		while (input.compareTo(current) < 0) {
-			previous = current;
-			current = current.getPrevious();
-		}
-		if (input.equals(current)) {
-			System.out.println("Already on List");
-		} else {
-			input.setPrevious(current);
-			current.setNext(input);
-			previous.setPrevious(input);
-			input.setNext(previous);
-		}
-	}
+    public void backwardsAdd(ListNode<E> input) {
+        ListNode<E> current = back;
+        ListNode<E> previous = null;
+        while (input.compareTo(current) < 0) {
+            previous = current;
+            current = current.getPrevious();
+        }
+        if (input.equals(current)) {
+            System.out.println("Already on List");
+        } else {
+            input.setPrevious(current);
+            current.setNext(input);
+            previous.setPrevious(input);
+            input.setNext(previous);
+        }
+    }
 
-	/**
-	 * Deletes a given ListNode object from the linked list, keeping the list in
-	 * order
-	 * 
-	 * @param input
-	 *            ListNode object to be removed if present
-	 */
-	public void delete(ListNode<E> input) {
-		if (front == null) {
-			System.out.println("List Empty");
-		} else if (input.equals(front)) {
-			front = front.getNext();
-		} else {
-			ListNode<E> current = front;
-			ListNode<E> previous = null;
-			while (current.getNext() != null && !input.equals(current)) {
-				previous = current;
-				current = current.getNext();
-			}
-			if (input.equals(current)) {
-				if (current.getNext() == null) {
-					previous.setNext(null);
-				} else {
-					previous.setNext(current.getNext());
-				}
-			} else {
-				System.out.println("Not on List");
-			}
-		}
-	}
+    /**
+     * Deletes a given ListNode object from the linked list, keeping the list in
+     * order
+     *
+     * @param input ListNode object to be removed if present
+     */
+    public void delete(ListNode<E> input) {
+        if (front == null) {
+            System.out.println("List Empty");
+        } else if (input.equals(front)) {
+            front = front.getNext();
+        } else {
+            ListNode<E> current = front;
+            ListNode<E> previous = null;
+            while (current.getNext() != null && !input.equals(current)) {
+                previous = current;
+                current = current.getNext();
+            }
+            if (input.equals(current)) {
+                if (current.getNext() == null) {
+                    previous.setNext(null);
+                } else {
+                    previous.setNext(current.getNext());
+                }
+            } else {
+                System.out.println("Not on List");
+            }
+        }
+    }
 
-	/**
-	 * Clears the entire linked list
-	 */
-	public void deleteList() {
-		front = null;
-		back = null;
-	}
+    /**
+     * Clears the entire linked list
+     */
+    public void deleteList() {
+        front = null;
+        back = null;
+    }
 
-	/**
-	 * Reverses the linked list
-	 * 
-	 * @param reverseThis
-	 *            front ListNode of the linked list to be reversed
-	 * @return ListNode object, only used in the method for recursion
-	 */
-	public ListNode<E> reverseList(ListNode<E> reverseThis) {
-		if (reverseThis == null || reverseThis.getNext() == null) {
-			front = reverseThis;
-			return reverseThis;
-		}
-		ListNode<E> temp = reverseList(reverseThis.getNext());
-		reverseThis.getNext().setNext(reverseThis);
-		reverseThis.setNext(null);
-		return temp;
-	}
+    /**
+     * Reverses the linked list
+     *
+     * @param reverseThis front ListNode of the linked list to be reversed
+     * @return ListNode object, only used in the method for recursion
+     */
+    public ListNode<E> reverseList(ListNode<E> reverseThis) {
+        if (reverseThis == null || reverseThis.getNext() == null) {
+            front = reverseThis;
+            return reverseThis;
+        }
+        ListNode<E> temp = reverseList(reverseThis.getNext());
+        reverseThis.getNext().setNext(reverseThis);
+        reverseThis.setNext(null);
+        return temp;
+    }
 
-	/**
-	 * Prints out the linked list in a readable format
-	 */
-	public void printList() {
-		if (front == null && back == null) {
-			System.out.println("List is Empty");
-		} else {
-			System.out.print("[ ");
-			ListNode<E> n = front;
-			while (n != null) {
-				System.out.print(n.getData() + " ");
-				n = n.getNext();
-			}
-			System.out.println("]");
-		}
-	}
+    /**
+     * Prints out the linked list in a readable format
+     */
+    public void printList() {
+        if (front == null && back == null) {
+            System.out.println("List is Empty");
+        } else {
+            System.out.print("[ ");
+            ListNode<E> n = front;
+            while (n != null) {
+                System.out.print(n.getData() + " ");
+                n = n.getNext();
+            }
+            System.out.println("]");
+        }
+    }
 
-	public void printReverseList() {
-		if (front == null && back == null) {
-			System.out.println("List is Empty");
-		} else {
-			System.out.print("[ ");
-			ListNode<E> n = back;
-			while (n != null) {
-				System.out.print(n.getData() + " ");
-				n = n.getPrevious();
-			}
-			System.out.println("]");
-		}
+    public void printReverseList() {
+        if (front == null && back == null) {
+            System.out.println("List is Empty");
+        } else {
+            System.out.print("[ ");
+            ListNode<E> n = back;
+            while (n != null) {
+                System.out.print(n.getData() + " ");
+                n = n.getPrevious();
+            }
+            System.out.println("]");
+        }
 
-	}
+    }
 
-	public static void main(String[] args) {
-		LinkedListProgram<Integer> asdf = new LinkedListProgram<Integer>();
-		ListNode<Integer> mid = new ListNode<Integer>(5, null, null);
-		asdf.add(new ListNode<Integer>(1, null, null), mid);
-		asdf.add(new ListNode<Integer>(2, null, null), mid);
-		asdf.add(new ListNode<Integer>(3, null, null), mid);
-		asdf.add(new ListNode<Integer>(4, null, null), mid);
-		asdf.add(new ListNode<Integer>(5, null, null), mid);
-		asdf.add(new ListNode<Integer>(6, null, null), mid);
-		asdf.add(new ListNode<Integer>(7, null, null), mid);
-		asdf.add(new ListNode<Integer>(8, null, null), mid);
-		asdf.add(new ListNode<Integer>(9, null, null), mid);
-		asdf.add(new ListNode<Integer>(10, null, null), mid);
+    public static void main(String[] args) {
+        LinkedListProgram<Integer> asdf = new LinkedListProgram<Integer>();
+        ListNode<Integer> mid = new ListNode<Integer>(5, null, null);
+        asdf.add(new ListNode<Integer>(1, null, null), mid);
+        asdf.add(new ListNode<Integer>(2, null, null), mid);
+        asdf.add(new ListNode<Integer>(3, null, null), mid);
+        asdf.add(new ListNode<Integer>(4, null, null), mid);
+        asdf.add(new ListNode<Integer>(5, null, null), mid);
+        asdf.add(new ListNode<Integer>(6, null, null), mid);
+        asdf.add(new ListNode<Integer>(7, null, null), mid);
+        asdf.add(new ListNode<Integer>(8, null, null), mid);
+        asdf.add(new ListNode<Integer>(9, null, null), mid);
+        asdf.add(new ListNode<Integer>(10, null, null), mid);
 
-		System.out.println("Original List:");
-		asdf.printList();
-		System.out.println("Reversed List:");
-		asdf.printReverseList();
-	}
+        System.out.println("Original List:");
+        asdf.printList();
+        System.out.println("Reversed List:");
+        asdf.printReverseList();
+    }
 
 }
--- src/Person.java
+++ src/Person.java
@@ -1,31 +1,40 @@
 
 public class Person {
 
-	private String firstName;
-	private String lastName;
-	private String employer;
+    private String firstName;
+    private String lastName;
+    private String employer;
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public String getEmployer() {
-		return employer;
-	}
+    public String getEmployer() {
+        return employer;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public void setEmployer(String employer) {
-		this.employer = employer;
-	}
+    public void setEmployer(String employer) {
+        this.employer = employer;
+    }
+
+    public int compareTo(Person input) {
+        return this.lastName.compareTo(input.lastName);
+    }
+
+    public boolean equals(Person input) {
+        return this.lastName.equals(input.lastName);
+    }
+
 }
