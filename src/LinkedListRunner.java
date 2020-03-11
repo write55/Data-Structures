aa408793b1448b5d3b38e9c7e82e4aff85ac31f0
write55 <writeusernamehere55@gmail.com>
2020-03-10 20:00:33 -0400

    LinkedList Program

--- src/LinkedListRunner.java
+++ src/LinkedListRunner.java
@@ -6,25 +6,27 @@ import java.util.StringTokenizer;
 
 public class LinkedListRunner<E extends Comparable<E>> {
 
-	public void readFile() throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Enter relative path of file: ");
-		FileReader readFile = new FileReader(in.readLine());
-		BufferedReader inFile = new BufferedReader(readFile);
-		System.out.println("\nReading File...\n");
-		String input = inFile.readLine();
-		while (input != null) {
-			tokenizeString(input);
-			input = inFile.readLine();
-		}
-		System.out.println("Reading Complete.");
-		inFile.close();
-	}
+    public void readFile() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter relative path of file: ");
+        FileReader readFile = new FileReader(in.readLine());
+        BufferedReader inFile = new BufferedReader(readFile);
+        System.out.println("\nReading File...\n");
+        String input = inFile.readLine();
+        while (input != null) {
+            tokenizeString(input);
+            input = inFile.readLine();
+        }
+        System.out.println("Reading Complete.");
+        inFile.close();
+    }
 
-	private void tokenizeString(String input) {
-	        StringTokenizer st = new StringTokenizer(input);
-	        while (st.hasMoreTokens()) {
-	            ListNode<Person> temp = new ListNode<Person>(new Person(st.nextToken(), st.nextToken(), st.nextToken()), null,null);
-	        }
-	    }
+    private void tokenizeString(String input) {
+        StringTokenizer st = new StringTokenizer(input);
+        while (st.hasMoreTokens()) {
+            ListNode<Person> temp = new ListNode<Person>(new Person(st.nextToken(), st.nextToken(), st.nextToken()), null, null);
+        }
+    }
+
+
 }
--- src/Person.java
+++ src/Person.java
@@ -1,46 +1,46 @@
 
-public class Person {
+public class Person implements Comparable<Person> {
 
-	private String firstName;
-	private String lastName;
-	private String employer;
+    private String firstName;
+    private String lastName;
+    private String employer;
 
-	public Person(String firstName, String lastName, String employer) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.employer = employer;
-	}
+    public Person(String firstName, String lastName, String employer) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employer = employer;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public String getEmployer() {
-		return employer;
-	}
+    public String getEmployer() {
+        return employer;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public void setEmployer(String employer) {
-		this.employer = employer;
-	}
+    public void setEmployer(String employer) {
+        this.employer = employer;
+    }
 
-	public int compareTo(Person input) {
-		return this.lastName.compareTo(input.lastName);
-	}
+    public int compareTo(Person input) {
+        return this.lastName.compareTo(input.lastName);
+    }
 
-	public boolean equals(Person input) {
-		return this.lastName.equals(input.lastName);
-	}
+    public boolean equals(Person input) {
+        return this.lastName.equals(input.lastName);
+    }
 
 }
